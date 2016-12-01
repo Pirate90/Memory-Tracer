b5a95f47175a6577292c0765c3d642afe3d32f3c
pirate90 <game901221@gmail.com>
2016-12-01 14:19:56 +0900

    사용자 선택사항 추가

--- titlename.c
+++ titlename.c
@@ -5,57 +5,69 @@ int main(void)
 {
 	char title[10][21];
 	char genre[10][5];
-	int story_score[10];
-	int music_score[10];
-	int casting_score[10];
+	int story_score[10],music_score[10],casting_score[10];
 	char story[10][101];
 	char review[10][1001];
 	char pic1[10][100];
 	char pic2[10][100];
 	char pic3[10][100];
+	int user_choice;
 	int i;
 	int count=2;
 
-	for(i=0; i<count; i++) {
-		printf("제목을 입력하세요 (단, 한글 10자 이내) : ");
-		fgets(title[i], 21, stdin);
-		printf("장르를 입력하세요 (액션, SF, 멜로, 공포 중에서)) : ");
-		scanf("%s", genre[i]);
-		printf("스토리 점수를 입력하세요 (단, 0~10까지의 정수 입력) : ");
-		scanf("%d", &story_score[i]);
-	        printf("음악 점수를를 입력하세요 (단, 0~10까지의 정수 입력) : ");
-		scanf("%d", &music_score[i]);
-		printf("캐스팅 점수를 입력하세요 (단, 0~10까지의 정수 입력) : ");
-		scanf("%d", &casting_score[i]);
-		getchar();
-		printf("스토리를 입력하세요 (단, 한글 50자 이내) : ");
-		fgets(story[i], 101, stdin);
-		printf("느낀점을 입력하세요 (단, 한글 500자 이내) : ");
-		fgets(review[i], 1001, stdin);
+	while(1) {
+		printf("Memory Tracer에 오신것을 환영합니다.\n");
+		printf("메뉴를 선택하세요.\n");
+		printf("1. 새로운 감상문 입력하기\n");
+		printf("2. 감상문목록 관리하기\n");
+		printf("3. 추천 목록보기\n");
+		printf("4. 종료하기\n");
+
+		printf("당신의 선택은?\n");
+		scanf("%d", &user_choice);
+
+		if(user_choice == 1) {
+			for(i=0; i<count; i++) {
+				printf("제목을 입력하세요 (단, 한글 10자 이내) : ");
+				fgets(title[i], 21, stdin);
+				printf("장르를 입력하세요 (액션, SF, 멜로, 공포 중에서)) : ");
+				scanf("%s", genre[i]);
+				printf("스토리 점수를 입력하세요 (단, 0~10까지의 정수 입력) : ");
+				scanf("%d", &story_score[i]);
+					while(story_score[i]>10) {
+						printf("0~10사이의 정수의 값을 다시 입력해주세요!");
+						scanf("%d", &story_score[i]);
+					}
+				printf("음악 점수를를 입력하세요 (단, 0~10까지의 정수 입력) : ");
+				scanf("%d", &music_score[i]);
+		                        while(music_score[i]>10) {
+        		                        printf("0~10사이의 정수의 값을 다시 입력해주세요!");
+                		                scanf("%d", &music_score[i]);
+		                        }
+				printf("캐스팅 점수를 입력하세요 (단, 0~10까지의 정수 입력) : ");
+				scanf("%d", &casting_score[i]);
+        		                while(casting_score[i]>10) {
+                		                printf("0~10사이의 정수의 값을 다시 입력해주세요!");
+                		                scanf("%d", &casting_score[i]);
+                		        }
+				getchar();
+				printf("스토리를 입력하세요 (단, 한글 50자 이내) : ");
+				fgets(story[i], 101, stdin);
+				printf("느낀점을 입력하세요 (단, 한글 500자 이내) : ");
+				fgets(review[i], 1001, stdin);
+			}
+		}
+		else if(user_choice == 2) {
+			for(i=0; i<count; i++) {
+				printf("제목 :  %s\t,장르 : %s\t,스토리점수 : %d\t,음악점수 : %d\t,그림체점수 : %d\t,스토리 : %s\t,느낀점 : %s\t\n",title[i],genre[i],story_score[i],music_score[i],casting_score[i],story[i],review[i]);
+			}
+		}
+		else if(user_choice == 3) {
+			printf("추천하는 목록은 --- 입니다.");
+		}
+		else if(user_choice == 4) {
+			break;
+		}
 	}
-
-	//strcpy(title[0], "코드기어스");
-	//strcpy(genre[0], "S/F");
-	//story_score[0] = 10;
-	//music_score[0] = 8;
-	//casting_score[0] = 9;
-	//strcpy(story[0], "루루슈 킹왕짱 개쌤 기아스");
-	//strcpy(review[0], "우리 형이 좋아해요.");
-//	pic1 = "image/image1.png";
-//	pic2 = "image/image2.png";
-//	pic3 = "image/image3.png";
-	
-        //strcpy(title[1], "슈타인즈게이트");
-        //strcpy(genre[1], "S/F");
-        //story_score[1] = 10;
-        //music_score[1] = 7;
-        //casting_score[1] = 3;
-        //strcpy(story[1], "0과1");
-        //strcpy(review[1], "우리 형이 좋아해요2.");
-
-	for(i=0; i<count; i++) {
-		printf("제목 :  %s\t,장르 : %s\t,스토리점수 : %d\t,음악점수 : %d\t,그림체점수 : %d\t,스토리 : %s\t,느낀점 : %s\t\n",title[i],genre[i],story_score[i],music_score[i],casting_score[i],story[i],review[i]);
-	}
-
 	return 0;
 }
